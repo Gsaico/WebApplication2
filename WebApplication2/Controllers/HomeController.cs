a3d3180e3469689f949c870c0f402336c68f6b84
yoni <yoni@WINDOWS10PC>
2016-11-05 07:04:13 -0500

    mensaje alterado

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "quien soy yo.";
 
             return View();
         }
